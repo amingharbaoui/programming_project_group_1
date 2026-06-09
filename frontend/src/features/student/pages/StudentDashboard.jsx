06c08023ff2c50c469f50815ce97ecd5fd77bac4
mohamad-azhar <mohamad.sheikh.aleshra@student.ehb.be>
2026-06-09 14:37:01 +0200

    Stageaanvraagkaart toegevoegd

--- frontend/src/features/student/pages/StudentDashboard.jsx
+++ frontend/src/features/student/pages/StudentDashboard.jsx
@@ -1,3 +1,28 @@
 export default function StudentDashboard() {
-  return <h1>Student dashboard</h1>;
+  return( 
+  <>
+
+  <div className = "page-inner">
+    <div className = "page-header">
+      <h1>Mijn stage</h1>
+      <p>Academiejaar 2025-2026</p>
+    </div>
+    
+    <div className="card">
+      <div className="card-title">
+        <i className="ti ti-briefcase"/>
+        Stageaanvraag
+      </div>
+      <p>Je hebt nog geen stage. <span> Alles start met je stagevoorstel: bedrijf, mentor, opdracht en periode. Na indiening bekijkt de stagecommisie je voorstel</span> </p>
+      <div className="actions">
+        <button className="btn-primary">
+          Stagevoorstel indienen
+        </button>
+      </div>
+    </div>
+
+ 
+  </div>
+  </>
+  )
 }
