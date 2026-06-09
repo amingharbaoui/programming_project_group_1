4fee34a2725a2eb7a08866e199ec265a28bbed84
mohamad-azhar <mohamad.sheikh.aleshra@student.ehb.be>
2026-06-09 19:30:15 +0200

    popup toegevoegd

--- frontend/src/features/student/pages/StudentDashboard.jsx
+++ frontend/src/features/student/pages/StudentDashboard.jsx
@@ -1,11 +1,43 @@
-import { useNavigate } from "react-router-dom"
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom"
 
 export default function StudentDashboard() {
       const navigate = useNavigate();
+      const location = useLocation();
+      const [ingediend, setIngediend] = useState (location.state?.ingediend || false)
+      const [showPopup, setShowPopup] = useState (location.state?.ingediend || false)
       
+      function handleBegrepen(){
+        setShowPopup(false);
+
+      }
   return( 
 
   <div className = "page-inner">
+    {showPopup && (
+        <div className="popup-overlay">
+          <div className="popup">
+            <div className="popup-header">
+              <div className="card-title">
+                <i className="ti ti-send" />
+                Stagevoorstel ingediend
+              </div>
+              <button className="btn" onClick={handleBegrepen}>
+                <i className="ti ti-x" />
+              </button>
+            </div>
+            <div className="popup-body">
+              <p>Je stagevoorstel werd ingediend bij de stagecommissie. Je krijgt een melding na de beoordeling.</p>
+            </div>
+            <div className="actions">
+              <button className="btn primary" onClick={handleBegrepen}>
+                <i className="ti ti-check" />
+                Begrepen
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     <div className = "page-header">
       <h1>Mijn stage</h1>
       <p>Academiejaar 2025-2026</p>
