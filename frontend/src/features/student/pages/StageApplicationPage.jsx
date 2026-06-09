090085867568547f7768d57e27ac6cf9b501d64e
mohamad-azhar <mohamad.sheikh.aleshra@student.ehb.be>
2026-06-09 15:35:40 +0200

    Bedrijfsformulier toegevoegd

--- frontend/src/features/student/pages/StageApplicationPage.jsx
+++ frontend/src/features/student/pages/StageApplicationPage.jsx
@@ -7,6 +7,27 @@ export default function StageApplicationPage() {
         <p>Vul alles in - je kan tussentijds opslaan als concept</p>
       </div>
       
+      <div className="grid-2">
+        <div className="card-title">
+          <i className="ti ti-building" />
+              Bedrijf
+        </div>
+        <div className="form-row">
+          <div className="form-group">
+             <label className="form-label">Bedrijfsnaam<span className="req">*</span></label>
+            <input className="form-input" type="text" placeholder="Naam van het bedrijf" />
+          </div>
+          <div className="form-group">
+            <label className="form-label">Afdeling</label>
+            <input className="form-input" type="text" placeholder="Afdeling of team" />
+          </div>
+          <div className="form-group">
+              <label className="form-label">Adres<span className="req">*</span></label>
+              <input className="form-input" type="text" placeholder="Straat nr, postcode gemeente" required />
+          </div>
+
+        </div>
+      </div>
     </div>
     </>
   )
