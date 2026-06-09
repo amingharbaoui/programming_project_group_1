9aae090e5653f920a58e9daa9024f50eb8228776
Ehbnathanmadimba <lungeni.madimba.betukumesu@student.ehb.be>
2026-06-09 17:52:57 +0200

    Stagecommissie aanvragen pagina toegevoegd

--- frontend/src/features/committee/pages/ApplicationsPage.jsx
+++ frontend/src/features/committee/pages/ApplicationsPage.jsx
@@ -1,3 +1,34 @@
+import { useState } from "react";
+
 export default function ApplicationsPage() {
-  return <h1>Stagecommissie aanvragen</h1>;
+  const [applications] = useState([
+    {
+      id: 1,
+      student: "Nathan",
+      company: "Microsoft",
+      period: "01/09/2026 - 31/01/2027",
+      status: "Pending",
+      description: "Ontwikkeling van Stageify",
+    },
+  ]);
+
+  return (
+    <div>
+      <h1>Stageaanvragen</h1>
+
+      {applications.map((app) => (
+        <div key={app.id}>
+          <h3>{app.student}</h3>
+          <p>{app.company}</p>
+          <p>{app.period}</p>
+          <p>Status: {app.status}</p>
+          <p>{app.description}</p>
+
+          <button>Goedkeuren</button>
+          <button>Afkeuren</button>
+          <button>Aanpassing vragen</button>
+        </div>
+      ))}
+    </div>
+  );
 }
