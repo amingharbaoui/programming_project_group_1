157a65fb4f6acb4a0cdf706c5c57f838370c28e2
mohamad-azhar <mohamad.sheikh.aleshra@student.ehb.be>
2026-06-10 17:42:07 +0200

    Voeg centrale API-service toe

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "/api",
+  headers: {
+    "x-user-id": "1"
+  }
 });
+
+export async function apiRequest(method, url, data = null) {
+  const res = await api({ method, url, data });
+  return res.data;
+}
 
 export default api;
