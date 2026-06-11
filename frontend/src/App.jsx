14759a6d48d38980f9d073ae35eccdcb176084f9
mohamad-azhar <mohamad.sheikh.aleshra@student.ehb.be>
2026-06-11 11:56:25 +0200

    Dashbord

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,20 +9,16 @@ import MyInternshipPage from "./features/student/pages/MyInternshipPage";
 import StudentLogbookPage from "./features/student/pages/StudentLogbookPage";
 import StudentEvaluationPage from "./features/student/pages/StudentEvaluationPage";
 
-import CommitteeDashboard from "./features/committee/pages/CommitteeDashboard";
 import ApplicationsPage from "./features/committee/pages/ApplicationsPage";
 
-import AdminDashboard from "./features/admin/pages/AdminDashboard";
 import DossiersPage from "./features/admin/pages/DossiersPage";
 import UsersPage from "./features/admin/pages/UsersPage";
 import CompetenciesPage from "./features/admin/pages/CompetenciesPage";
 
-import MentorDashboard from "./features/mentor/pages/MentorDashboard";
 import MentorStudentsPage from "./features/mentor/pages/MentorStudentsPage";
 import MentorLogbooksPage from "./features/mentor/pages/MentorLogbooksPage";
 import MentorEvaluationPage from "./features/mentor/pages/MentorEvaluationPage";
 
-import DocentDashboard from "./features/docent/pages/DocentDashboard";
 import DocentStudentsPage from "./features/docent/pages/DocentStudentsPage";
 import DocentLogbooksPage from "./features/docent/pages/DocentLogbooksPage";
 import DocentEvaluationsPage from "./features/docent/pages/DocentEvaluationsPage";
@@ -42,20 +38,16 @@ export default function App() {
             <Route path="/student/logbook" element={<StudentLogbookPage />} />
             <Route path="/student/evaluation" element={<StudentEvaluationPage />} />
 
-            <Route path="/committee" element={<CommitteeDashboard />} />
             <Route path="/committee/applications" element={<ApplicationsPage />} />
 
-            <Route path="/admin" element={<AdminDashboard />} />
             <Route path="/admin/dossiers" element={<DossiersPage />} />
             <Route path="/admin/users" element={<UsersPage />} />
             <Route path="/admin/competencies" element={<CompetenciesPage />} />
 
-            <Route path="/mentor" element={<MentorDashboard />} />
             <Route path="/mentor/students" element={<MentorStudentsPage />} />
             <Route path="/mentor/logbooks" element={<MentorLogbooksPage />} />
             <Route path="/mentor/evaluation" element={<MentorEvaluationPage />} />
 
-            <Route path="/docent" element={<DocentDashboard />} />
             <Route path="/docent/students" element={<DocentStudentsPage />} />
             <Route path="/docent/logbooks" element={<DocentLogbooksPage />} />
             <Route path="/docent/evaluations" element={<DocentEvaluationsPage />} />
