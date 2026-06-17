67e3aff93f62a929a38dd1631a9fda6e5d740f35
David Vuylsteker <david.vuylsteker@student.ehb.be>
2026-06-17 18:07:26 +0200

    admin kan documenttype verwijderen (vaste beschermd) en mentor-uitnodiging opnieuw versturen

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -8,8 +8,8 @@ const {
   generateEindoverzicht,
   sendContractReminder
 } = require("../controllers/internshipController");
-const { getSettings, updateStageRule, updateDocumentType, createDocumentType } = require("../controllers/settingsController");
-const { inviteMentor } = require("../controllers/userController");
+const { getSettings, updateStageRule, updateDocumentType, createDocumentType, deleteDocumentType } = require("../controllers/settingsController");
+const { inviteMentor, resendInvitation } = require("../controllers/userController");
 const { approveDocument, rejectDocument } = require("../controllers/documentController");
 const { registerOvereenkomst } = require("../controllers/contractController");
 const { authenticateDemoUser, requireRole } = require("../middleware/authMiddleware");
@@ -31,8 +31,10 @@ router.get("/settings", getSettings);
 router.patch("/stage-rules/:id", updateStageRule);
 router.post("/document-types", createDocumentType);
 router.patch("/document-types/:id", updateDocumentType);
+router.delete("/document-types/:id", deleteDocumentType);
 
 router.post("/invitations", inviteMentor);
+router.post("/invitations/:id/resend", resendInvitation);
 
 router.patch("/documents/:id/approve", approveDocument);
 router.patch("/documents/:id/reject", rejectDocument);
